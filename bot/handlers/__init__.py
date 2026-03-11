8babb632082ec51e957809ffd490fe760dc5037a
Pavel Kislov <116721407+FrustrationDesu@users.noreply.github.com>
2026-03-11 23:58:31 +0300

    Add rewards service, DB schema, and reward command handlers

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,15 @@
+from .rewards import (
+    daily_command,
+    missions_command,
+    register_rewards_handlers,
+    rewards_command,
+    vip_command,
+)
+
+__all__ = [
+    "rewards_command",
+    "daily_command",
+    "missions_command",
+    "vip_command",
+    "register_rewards_handlers",
+]
